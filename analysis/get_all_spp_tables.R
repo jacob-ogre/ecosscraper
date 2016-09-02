7c71a18b6823e0ef5e7178d953e33ea31294db8e
Jacob Malcom <jmalcom@defenders.org>
2016-09-02 16:53:54 -0400

    Update get_all_spp_tables for mclapply.

--- analysis/get_all_spp_tables.R
+++ analysis/get_all_spp_tables.R
@@ -3,31 +3,43 @@
 library(dplyr)
 library(ecosscraper)
 library(parallel)
-# library(rvest)
-# library(tibble)
-# library(xml2)
 
 data("TECP_domestic")
 
-species <- TECP_domestic$Species_Page[1:5]
-test <- lapply(species, get_tables)
-names(test) <- TECP_domestic$Scientific_Name[1:5]
-length(test)
-lapply(test, names)
+# Let's try to get all of the ECOS tables...for domestic species, at least
+species <- TECP_domestic$Species_Page
+ECOS_tabs <- parallel::mclapply(species, get_tables, mc.cores = 6)
+names(ECOS_tabs) <- TECP_domestic$Scientific_Name
+save(ECOS_tabs, file = "data/ECOS_tabs.rda")
 
-test_SP_TAB <- distinct(bind_tables(test, "SP_TAB"))
-test_FR_TAB <- distinct(bind_tables(test, "FR_TAB"))
-test_CH_TAB <- distinct(bind_tables(test, "CH_TAB"))
-test_REC_TAB <- distinct(bind_tables(test, "REC_TAB"))
+test_SP_TAB <- distinct(bind_tables(ECOS_tabs, "SP_TAB"))
+test_FR_TAB <- distinct(bind_tables(ECOS_tabs, "FR_TAB"))
+test_CH_TAB <- distinct(bind_tables(ECOS_tabs, "CH_TAB"))
+test_REC_TAB <- distinct(bind_tables(ECOS_tabs, "REC_TAB"))
 
-# Try the parallel version...what will break???
-species <- TECP_domestic$Species_Page[1:5]
-test <- parallel::mclapply(species, get_tables, mc.cores = 2)
-names(test) <- TECP_domestic$Scientific_Name[1:5]
-length(test)
-lapply(test, names)
+##########
+# Testing
+#
+# species <- TECP_domestic$Species_Page[1:5]
+# test <- lapply(species, get_tables)
+# names(test) <- TECP_domestic$Scientific_Name[1:5]
+# length(test)
+# lapply(test, names)
+# 
+# test_SP_TAB <- distinct(bind_tables(test, "SP_TAB"))
+# test_FR_TAB <- distinct(bind_tables(test, "FR_TAB"))
+# test_CH_TAB <- distinct(bind_tables(test, "CH_TAB"))
+# test_REC_TAB <- distinct(bind_tables(test, "REC_TAB"))
+# 
+# # Try the parallel version...what will break???
+# species <- TECP_domestic$Species_Page[1:5]
+# test <- parallel::mclapply(species, get_tables, mc.cores = 2)
+# names(test) <- TECP_domestic$Scientific_Name[1:5]
+# length(test)
+# lapply(test, names)
+# 
+# test_SP_TAB <- distinct(bind_tables(test, "SP_TAB"))
+# test_FR_TAB <- distinct(bind_tables(test, "FR_TAB"))
+# test_CH_TAB <- distinct(bind_tables(test, "CH_TAB"))
+# test_REC_TAB <- distinct(bind_tables(test, "REC_TAB"))
 
-test_SP_TAB <- distinct(bind_tables(test, "SP_TAB"))
-test_FR_TAB <- distinct(bind_tables(test, "FR_TAB"))
-test_CH_TAB <- distinct(bind_tables(test, "CH_TAB"))
-test_REC_TAB <- distinct(bind_tables(test, "REC_TAB"))
