e2020cefa4f3a4e60e1cb21036d2ae621d4b23f2
Jacob Malcom <jmalcom@defenders.org>
2016-09-02 20:11:03 -0400

    Add in the scraped tables for domestic species.

--- analysis/get_all_spp_tables.R
+++ analysis/get_all_spp_tables.R
@@ -8,15 +8,26 @@ data("TECP_domestic")
 
 # Let's try to get all of the ECOS tables...for domestic species, at least
 species <- unique(TECP_domestic$Species_Page)
-ECOS_tabs <- parallel::mclapply(species, get_tables, mc.cores = 6)
-# ECOS_tabs <- lapply(species, get_tables)
+# ECOS_tabs <- parallel::mclapply(species, get_tables, mc.cores = 6)
+ECOS_tabs <- lapply(species, get_tables)
 names(ECOS_tabs) <- unique(TECP_domestic$Scientific_Name)
 save(ECOS_tabs, file = "data-raw/ECOS_tabs.rda")
 
-test_SP_TAB <- distinct(bind_tables(ECOS_tabs, "SP_TAB"))
-test_FR_TAB <- distinct(bind_tables(ECOS_tabs, "FR_TAB"))
-test_CH_TAB <- distinct(bind_tables(ECOS_tabs, "CH_TAB"))
-test_REC_TAB <- distinct(bind_tables(ECOS_tabs, "REC_TAB"))
+species_info_table <- distinct(bind_tables(ECOS_tabs, "SP_TAB"))
+federal_register_table <- distinct(bind_tables(ECOS_tabs, "FR_TAB"))
+critical_habitat_table <- distinct(bind_tables(ECOS_tabs, "CH_TAB"))
+recovery_plan_table <- distinct(bind_tables(ECOS_tabs, "REC_TAB"))
+add_document_table <- distinct(bind_tables(ECOS_tabs, "DOC_TAB"))
+five_year_review_table <- distinct(bind_tables(ECOS_tabs, "REV_TAB"))
+scraping_table <- distinct(bind_tables(ECOS_tabs, "scrape_info"))
+
+save(species_info_table, file = "data/species_info_table.rda")
+save(federal_register_table, file = "data/federal_register_table.rda")
+save(critical_habitat_table, file = "data/critical_habitat_table.rda")
+save(recovery_plan_table, file = "data/recovery_plan_table.rda")
+save(add_document_table, file = "data/add_document_table.rda")
+save(five_year_review_table, file = "data/five_year_review_table.rda")
+save(scraping_table, file = "data/scraping_table.rda")
 
 ##########
 # Testing
