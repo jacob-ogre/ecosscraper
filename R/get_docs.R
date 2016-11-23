c1eeb20c5bbe2ebba8ceee19e6f79f9b5691c0fb
Jacob Malcom <jacob.w.malcom@gmail.com>
2016-11-23 10:37:40 -0500

    Fix up the document getter.

--- R/get_docs.R
+++ R/get_docs.R
@@ -2,7 +2,7 @@
 #'
 #' Simple function to download a file from ECOS.
 #'
-#' @param link A URL from ECOS to download a document
+#' @param url A URL from ECOS to download a document
 #' @param subd Subdirectory to which the document will be downloaded
 #' @param pause Whether to pause for 0.5-3 seconds during scraping
 #' @return A data.frame with destination and success 
@@ -11,28 +11,40 @@
 #' # one or more lines to demo the function
 get_document <- function(url, subd, pause = TRUE) {
   dest <- make_pdf_dest(url, subd)
-  if(!file.exists(dest) | !is_pdf(dest)) {
+  url <- URLencode(url)
+  if(!file_check(dest)) {
     if(pause == TRUE) Sys.sleep(runif(1, 0.5, 3))
-    if(httr::http_error(url))
-    res <- try(httr::GET(URLencode(url), 
-                         httr::write_disk(dest, overwrite = TRUE)))
-    if(class(res) == "try-error") {
-      # Try once more because the error may be a temp connection issue
-      res <- try(httr::GET(URLencode(url), 
-                           httr::write_disk(dest, overwrite = TRUE)))
-      if(class(res) == "try-error") {
-        print(paste("GET Error:", res))
-        return(data.frame(dest = dest, 
-                          success = "Failed", 
-                          stringsAsFactors = FALSE))
+    if(!httr::http_error(url)) {
+      res <- try(httr::GET(url, httr::write_disk(dest, overwrite = TRUE)))
+      if(class(res) == "try-error") { # Try once more 
+        res <- try(httr::GET(url, httr::write_disk(dest, overwrite = TRUE)))
+        if(class(res) == "try-error") {
+          return(data.frame(url = url
+                            dest = dest, 
+                            success = "Failed", 
+                            pdfCheck = NA,
+                            stringsAsFactors = FALSE))
+        }
       }
+      pdfCheck <- is_pdf(dest)
+      return(data.frame(url = url,
+                        dest = dest, 
+                        success = "Success", 
+                        pdfCheck = pdfCheck,
+                        stringsAsFactors = FALSE))
+    } else {
+      return(data.frame(url = url,
+                        dest = dest, 
+                        success = "Failed", 
+                        pdfCheck = NA,
+                        stringsAsFactors = FALSE))
     }
-    return(data.frame(dest = dest, 
-                      success = "Success", 
-                      stringsAsFactors = FALSE))
   } else {
-    return(data.frame(dest = dest, 
+    pdfCheck <- is_pdf(dest)
+    return(data.frame(url = url,
+                      dest = dest, 
                       success = "Pre-exist", 
+                      pdfCheck = pdfCheck,
                       stringsAsFactors = FALSE))
   }
 }
@@ -46,10 +58,25 @@ get_document <- function(url, subd, pause = TRUE) {
 make_pdf_dest <- function(link, subd = "") {
   fname <- basename(link)
   outf <- ifelse(grepl(fname, pattern = "pdf$"), fname, paste0(fname, ".pdf"))
-  outf <- gsub(outf, pattern = " ", "_")
+  outf <- gsub(outf, pattern = " ", replacement = "_")
   if(!dir.exists(subd)) dir.create(subd, recursive = TRUE)
   dest <- file.path(subd, outf)
   return(dest)
+}
+
+#' Check if file exists and if so, if it is really a PDF
+#' 
+#' @param f File to check
+#' @return TRUE if file exists and is PDF, else FALSE
+#' @export
+file_check <- function(f) {
+  if(file.exists(f)) {
+    if(is_pdf(f)) {
+      return(TRUE)
+    }
+    return(FALSE)
+  }
+  return(FALSE)
 }
 
 #' Test if a file is a pdf
@@ -58,8 +85,11 @@ make_pdf_dest <- function(link, subd = "") {
 #' @return TRUE if pdftools::pdf_info thinks it's a PDF, else FALSE
 #' @export
 is_pdf <- function(f) {
-  res <- try(pdftools::pdf_info(f))
-  if(class(res) != "try-error") return(TRUE)
+  if(file.exists(f)) {
+    res <- suppressMessages(try(pdftools::pdf_info(f), silent = TRUE))
+    if(class(res) != "try-error") return(TRUE)
+    return(FALSE)
+  }
   return(FALSE)
 }
 
